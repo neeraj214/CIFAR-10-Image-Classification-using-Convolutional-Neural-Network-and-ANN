0a6c158ca6135cfa1a9f7b21b25563f100c33cbd
neeraj214 <150280096+neeraj214@users.noreply.github.com>
2026-05-28 00:00:58 +0530

    feat: add App and main React components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import ImageClassifier from './components/ImageClassifier'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950">
+      <ImageClassifier />
+    </div>
+  )
+}
+
+export default App
